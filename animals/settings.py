9d1a0ceea5cd98ec410e4aa203ef5cbe710f1733
maniblitz <tibakevin@gmail.com>
2019-06-20 15:40:51 -0400

    ssl mode update

--- animals/settings.py
+++ animals/settings.py
@@ -151,4 +151,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import django_heroku
 
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
